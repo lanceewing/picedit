8ddce3517993c367ab4483dc9ec0b42a26b36a83
lance.ewing@gmail.com <lance.ewing@gmail.com@f2a17653-8437-f213-5d11-6e590effad49>
2011-05-27 07:35:15 +0000

    Initial basic implementation of the display of the priority bands. TODO: Needs to be translucent to support background image.

--- picedit/src/com/agifans/picedit/PictureType.java
+++ picedit/src/com/agifans/picedit/PictureType.java
@@ -7,7 +7,45 @@ package com.agifans.picedit;
  */
 public enum PictureType {
 
-	AGI,
-	SCI0;
+	AGI(320, 168),
+	SCI0(320, 200);
+
+	/**
+	 * The width of the picture in pixels.
+	 */
+	private int width;
+
+	/**
+	 * The height of the picture in pixels.
+	 */
+	private int height;
 	
+	/**
+	 * Constructor for PictureType.
+	 * 
+	 * @param width The width of the picture in pixels.
+	 * @param height The height of the picture in pixels.
+	 */
+	PictureType(int width, int height) {
+	  this.width = width;
+	  this.height = height;
+	}
+	
+	/**
+	 * Gets the width of the picture in pixels.
+	 * 
+	 * @return the width of the picture in pixels.
+	 */
+	public int getWidth() {
+	  return width;
+	}
+	
+	/**
+	 * Gets the height of the picture in pixels.
+	 * 
+	 * @return the height of the picture in pixels.
+	 */
+	public int getHeight() {
+	  return height;
+	}
 }
