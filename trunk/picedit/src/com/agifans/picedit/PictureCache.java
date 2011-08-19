a6ba42e6cc5895a3c979fcdf0418bccdbba04dfb
lance.ewing@gmail.com <lance.ewing@gmail.com@f2a17653-8437-f213-5d11-6e590effad49>
2011-08-19 17:14:05 +0000

    Continued to build the picture cache.

--- trunk/picedit/src/com/agifans/picedit/PictureCache.java
+++ trunk/picedit/src/com/agifans/picedit/PictureCache.java
@@ -15,19 +15,19 @@ import java.util.TreeMap;
  * @author Lance Ewing
  */
 public class PictureCache {
-    
+
     /**
      * A map of picture position to 
      */
     private TreeMap<Integer, PictureCacheEntry> cache;
-    
+
     /**
      * Constructor for PictureCache.
      */
     public PictureCache() {
         this.cache = new TreeMap<Integer, PictureCacheEntry>();
     }
-    
+
     // Some notes about when the cache will be used or updated.
     //
     //    1. Load picture: Obviously has to draw the new picture from the start.
@@ -41,37 +41,52 @@ public class PictureCache {
     //    9. New picture: Clear all cached images and draw blank screen.
     //    10. Slider position change: Use cached image.
     //    11. Drawing actions use Picture to draw that action only, on top of the current picture. Needs to cache every position from that point to the end? (or invalidate the cache from that point to the end)
-    
-    
+
     /**
      * An entry in the picture cache is of this type. It contains the screen
      * data as it is at the associated picture position and also a subset of
      * the EditStatus attributes that are relevant to that picture position.
      */
     public class PictureCacheEntry {
-      
-      // TODO: This needs to store the visual, priority and control line screens.
-      // TODO: Should this store the int arrays or the associated BufferedImage?
 
-      
-      // This is the subset of data from the EditStatus that the Picture class
-      // alters when drawing the picture. For this reason it needs to be cached
-      // along with the screen data. This is so that the EditStatus can be adjusted
-      // to match the given picture position.
-      
-      private ToolType tool;
+        /**
+         * Holds the pixel data for the visual screen of the picture.
+         */
+        private int visualScreen[];
 
-      private int visualColour;
+        /**
+         * Holds the pixel data for the priority screen of the picture.
+         */
+        private int priorityScreen[];
 
-      private int priorityColour;
+        /**
+         * Holds the pixel data for the control screen of the picture.
+         */
+        private int controlScreen[];
+        
+        // TODO: This needs to store the visual, priority and control line screens.
+        // TODO: Should this store the int arrays or the associated BufferedImage?
 
-      private int controlColour;
-      
-      private int brushSize;
+        // This is the subset of data from the EditStatus that the Picture class
+        // alters when drawing the picture. For this reason it needs to be cached
+        // along with the screen data. This is so that the EditStatus can be adjusted
+        // to match the given picture position.
 
-      private BrushShape brushShape;
+        private ToolType tool;
 
-      private BrushTexture brushTexture;
-      
+        private int visualColour;
+
+        private int priorityColour;
+
+        private int controlColour;
+
+        private int brushSize;
+
+        private BrushShape brushShape;
+
+        private BrushTexture brushTexture;
+
+        
+        
     }
 }
