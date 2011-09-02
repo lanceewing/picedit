6d4cd23465880602de6ba1a80ab53dca9c12c537
lance.ewing@gmail.com <lance.ewing@gmail.com@f2a17653-8437-f213-5d11-6e590effad49>
2011-09-02 18:22:20 +0000

    Started experimenting with a desktop and internal frame to support multiple pictures.

---
+++ trunk/picedit/src/com/agifans/picedit/PictureFrame.java
@@ -0,0 +1,78 @@
+package com.agifans.picedit;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+
+/**
+ * 
+ * 
+ * @author Lance Ewing
+ */
+public class PictureFrame extends JInternalFrame {
+
+    /**
+     * The scroll pane that holds the picture panel.
+     */
+    private JScrollPane pictureScrollPane;
+    
+    /**
+     * The panel containing the picture being edited.
+     */
+    private PicturePanel picturePanel;
+    
+    /**
+     * 
+     */
+    private EditStatus editStatus;
+    
+    /**
+     * Constructor for PictureFrame.
+     * 
+     * @param editStatus 
+     * 
+     * TODO: Move creation of the PicturePanel into this class.
+     * TODO: Move creation of EditStatus, PicGraphics, Picture into this class.
+     */
+    public PictureFrame(EditStatus editStatus, PicturePanel picturePanel) {
+        this.editStatus = editStatus;
+        
+        this.picturePanel = picturePanel;
+        
+        // Add the panel that holds the picture that is being edited.
+        pictureScrollPane = new JScrollPane(picturePanel);
+        pictureScrollPane.setMinimumSize(new Dimension(10, 10));
+        pictureScrollPane.setOpaque(true);
+        pictureScrollPane.setBackground(Color.lightGray);
+        this.getContentPane().add(pictureScrollPane, BorderLayout.CENTER);
+        this.pack();
+        this.setMaximizable(true);
+        this.setIconifiable(true);
+        this.setResizable(true);
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.setVisible(true);
+    }
+    
+    /**
+     * Paints the PICEDIT applet.
+     */
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        // Make sure the slider is up to date with the picture position.
+        //positionSlider.getModel().setValue(picture.getPicturePosition());
+        
+        // If we are in a window then update the title to show the current picture name.
+        if (editStatus.getPictureFile() == null) {
+            this.setTitle("Untitled");
+        } else {
+            this.setTitle(editStatus.getPictureFile().getName());
+        }
+    }
+}
