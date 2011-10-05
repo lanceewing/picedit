63897bc7a160e760f866982f7267d5fab34b0a89
lance.ewing@gmail.com <lance.ewing@gmail.com@f2a17653-8437-f213-5d11-6e590effad49>
2011-10-05 07:26:15 +0000

    Added bevels to pop up dialogs

--- trunk/picedit/src/com/agifans/picedit/ColourChooserDialog.java
+++ trunk/picedit/src/com/agifans/picedit/ColourChooserDialog.java
@@ -9,6 +9,7 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
 
 /**
  * Colour chooser dialog used when selecting visual, priority and control colours.
@@ -38,20 +39,7 @@ public class ColourChooserDialog extends JDialog {
         this.setAlwaysOnTop(true);
         this.setLayout(null);
         
-        JPanel palettePanel = new JPanel() {
-            public void paintComponent(Graphics graphics) {
-                super.paintComponents(graphics);
-                int colourCode = 0;
-                for (int row = 0; row < 64; row = row + 16) {
-                    for (int column=0; column < 64; column = column + 16) {
-                        graphics.setColor(EgaPalette.COLOR_OBJECTS[colourCode++]);
-                        graphics.fillRect(column, row, 16, 16);
-                    }
-                }
-            }
-        };
-        palettePanel.setSize(new Dimension(64, 64));
-        palettePanel.setLocation(2, 2);
+        ColourChooserPanel palettePanel = new ColourChooserPanel();
         this.add(palettePanel);
         
         palettePanel.addMouseListener(new ColourChooserMouseHandler());
@@ -64,6 +52,29 @@ public class ColourChooserDialog extends JDialog {
      */
     public int getChosenColour() {
         return chosenColour;
+    }
+    
+    /**
+     * The panel that holds the colour palette.
+     */
+    class ColourChooserPanel extends JPanel {
+      
+        ColourChooserPanel() {
+            this.setSize(new Dimension(64, 64));
+            this.setLocation(2, 2);
+            this.setBorder(new BevelBorder(BevelBorder.RAISED));
+        }
+      
+        public void paintComponent(Graphics graphics) {
+            super.paintComponents(graphics);
+            int colourCode = 0;
+            for (int row = 0; row < 64; row = row + 16) {
+                for (int column=0; column < 64; column = column + 16) {
+                    graphics.setColor(EgaPalette.COLOR_OBJECTS[colourCode++]);
+                    graphics.fillRect(column, row, 16, 16);
+                }
+            }
+        }
     }
     
     /**
