b75bb05bf8358bc63cbc46949c838489803df9df
lance.ewing@gmail.com <lance.ewing@gmail.com@f2a17653-8437-f213-5d11-6e590effad49>
2011-10-10 21:03:43 +0000

    Removed view data option and tidied up the help page to make it accurate.

--- trunk/picedit/src/com/agifans/picedit/StatusBarPanel.java
+++ trunk/picedit/src/com/agifans/picedit/StatusBarPanel.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 import javax.swing.border.BevelBorder;
